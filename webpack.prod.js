c77dea6d0bbef7b6be1629b56025fc57a452e6fc
vika <55395608+finchik@users.noreply.github.com>
2023-08-29 15:40:33 +0300

    Add files via upload

---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({})
+    ],
+  },
+});
